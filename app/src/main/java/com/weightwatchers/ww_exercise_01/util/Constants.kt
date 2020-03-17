31745f01a459602e752bcfc81ee548897794bb8e
Apurvi Patel <apurvi.patel@cellpointmobile.com>
2020-03-17 13:58:17 -0500

    WW_code challenge

---
+++ app/src/main/java/com/weightwatchers/ww_exercise_01/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.weightwatchers.ww_exercise_01.util
+
+class Constants {
+    companion object{
+        const val CONNECTION_TIMEOUT = 60L
+        const val READ_TIMEOUT = 60L
+        const val API_BASE_URL = "https://www.weightwatchers.com/"
+    }
+}
